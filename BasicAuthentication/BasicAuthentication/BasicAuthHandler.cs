112aa0b3c9cd02f9b031ae631cf81023a9335653
AmanShankar <aman.shankar@abylle.com>
2023-09-21 12:13:16 +0530

    Basic_Authentication

---
+++ BasicAuthentication/BasicAuthentication/BasicAuthHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Text; // It provides classes for encoding and decoding character sets, including the Encoding class used later in the code.
+
+namespace BasicAuthentication
+{
+    public class BasicAuthHandler
+    {
+        //RequestDelegate is a delegate used to represent middleware in ASP.NET Core
+        private readonly RequestDelegate next;
+        private readonly string relm;
+        public BasicAuthHandler(RequestDelegate next , string relm)
+        {
+            this.next = next;
+            this.relm = relm;
+        }
+
+        public async Task InvokeAsync(HttpContext context)  // Used to handle HTTP requests
+        {
+            if (!context.Request.Headers.ContainsKey("Authorization"))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            //Basic userid:password
+            var header = context.Request.Headers["Authorization"].ToString();  //retrieves the "Authorization" header from the HTTP request
+            //The "Authorization" header value often starts with the word "Basic," followed by a space and the Base64-encoded credentials
+            var encoderdCreds = header.Substring(6);
+            //It decodes the Base64-encoded credentials into a plain text string, containing the format "username:password"
+            var creds = Encoding.UTF8.GetString(Convert.FromBase64String(encoderdCreds)); 
+            string[] uidpwd = creds.Split(':');
+            var uid = uidpwd[0]; //extracted username to variables
+            var password = uidpwd[1]; //extracted password to variables
+
+            if (uid != "aman" || password != "password")
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            await next(context);
+        }
+    }
+}
---
+++ BasicAuthentication/BasicAuthentication/Program.cs
@@ -0,0 +1,29 @@
+using BasicAuthentication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//UseMiddleware is used to add middleware components to the pipeline annd type of middleware is BasicAuthHandler
+app.UseMiddleware<BasicAuthHandler>("Test");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
