59049bffcd64fe35f94dd413ca5e374e222ffc50
AmanShankar <aman.shankar@abylle.com>
2023-09-21 17:24:15 +0530

    Token Based Authentication

---
+++ JWT_TokenBased_Authentication/JWT_TokenBased_Authentication/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_TokenBased_Authentication.Models
+{
+    public class Users
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
